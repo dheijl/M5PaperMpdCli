49b116ac38ceeceef4b0c01a9f76e9cc803d48fd
dheijl <danny.heijl@telenet.be>
2023-08-06 19:55:44 +0200

    Update M5PaperMpdCli.ino
    cleanup

--- M5PaperMpdCli/M5PaperMpdCli.ino
+++ M5PaperMpdCli/M5PaperMpdCli.ino
@@ -14,13 +14,13 @@ void setup()
     uint8_t reason = M5.RTC.readReg(0x01);
     // now it's safe
     M5.begin();
-    // check reboot reason flag
+    // check reboot reason flag: TIE (timer int enable) && TF (timer flag active)
     if ((reason & 0b0000101) == 0b0000101) {
         restartByRTC = true;
         Serial.println("Reboot by RTC");
     } else {
         restartByRTC = false;
-        Serial.println("Restart by power button");
+        Serial.println("Reboot by power button / USB");
     }
     M5.EPD.SetRotation(90);
     M5.TP.SetRotation(90);
@@ -30,12 +30,12 @@ void setup()
     canvas.createCanvas(540, 960);
     canvas.setTextSize(3);
     if (restartByRTC)
-        canvas.drawString("Power on by: RTC timer", 25, 250); // <= this one we should when waking up from sleep
+        canvas.drawString("Power on by RTC timer", 20, 250); // <= this one we should when waking up from sleep
     else
-        canvas.drawString("Power on by: PWR Btn", 25, 250);
+        canvas.drawString("Power on by PWR Btn / USB", 20, 250);
 
-    canvas.drawString("Press PWR Btn for sleep!", 45, 350);
-    canvas.drawString("Wakeup after 10 seconds!", 70, 450);
+    canvas.drawString("Press BtnR for sleep!", 40, 350);
+    canvas.drawString("Wakeup after 5 seconds!", 50, 450);
     canvas.pushCanvas(0, 0, UPDATE_MODE_DU4);
 }
 
@@ -44,20 +44,21 @@ void loop()
     if (M5.BtnR.wasPressed()) {
         canvas.drawString("I'm going to sleep.zzzZZZ~", 45, 550);
         canvas.pushCanvas(0, 0, UPDATE_MODE_DU4);
-        delay(1000);
+        delay(500);
         // this only disables MainPower, a NO-OP when on USB power
-        M5.shutdown(10); // shut down now and wake up after 10 seconds if on battery
+        M5.shutdown(5); // shut down now and wake up after 5 seconds if on battery
         // in case of USB power present: save power and wait for external RTC wakeup
         M5.disableEPDPower(); // digitalWrite(M5EPD_EPD_PWR_EN_PIN, 0);
         M5.disableEXTPower(); // digitalWrite(M5EPD_EXT_PWR_EN_PIN, 0);
-        esp_deep_sleep(11000000L);
+        esp_deep_sleep(5100000L);
     } else if (M5.BtnL.wasPressed()) {
+        // proper shutdown without wake-up (if not on USB power)
         M5.RTC.clearIRQ();
         M5.RTC.disableIRQ();
         M5.disableEPDPower(); // digitalWrite(M5EPD_EPD_PWR_EN_PIN, 0);
         M5.disableEXTPower(); // digitalWrite(M5EPD_EXT_PWR_EN_PIN, 0);
         M5.disableMainPower();
-        esp_deep_sleep(11000000L);
+        esp_deep_sleep(5100000L);
     }
     M5.update();
     delay(100);
